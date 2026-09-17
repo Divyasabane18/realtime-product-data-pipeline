d29860164f85f910e5f407ddca5dc257db4fee64
Divyasaban18 <divyasabane@gmail.com>
2026-09-17 09:12:19 +0530

    Added kafka consumer for product topic

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 import json 
 
 consumer = KafkaConsumer(
-    "product-topic",
+    "Producer_topic",
     bootstrap_servers="localhost:9092",
     auto_offset_reset="earliest",
     group_id="product_consumer_group",
