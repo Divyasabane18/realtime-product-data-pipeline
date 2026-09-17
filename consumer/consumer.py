51aee5ca7aeb8f8ea2400496d01ee2de9acd0edd
Divyasaban18 <divyasabane@gmail.com>
2026-09-17 19:38:58 +0530

    Implement Avro Kafka product pipeline

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,15 +1,26 @@
 from kafka import KafkaConsumer
 import json 
+from fastavro import parse_schema, schemaless_reader
+from io import BytesIO
+
+with open("../schemas/product.avsc", "r") as file:
+    schema = json.load(file)
+
+    parsed_schema = parse_schema(schema)
+
+    print("Avro schema loaded successfully !")
 
 consumer = KafkaConsumer(
-    "Producer_topic",
+    "product_updates",
     bootstrap_servers="localhost:9092",
     auto_offset_reset="earliest",
-    group_id="product_consumer_group",
-    value_deserializer=lambda x:json.loads(x.decode("utf-8"))
+    group_id="avro_product_consumer_group",
+    
 )
 
 print("Connected to kafka topic")
 
 for message in consumer:
-    print(message.value)
+    buffer = BytesIO(message.value)
+    product = schemaless_reader(buffer, parsed_schema)
+    print("Received product: ", product)
--- producer/producer.py
+++ producer/producer.py
@@ -2,7 +2,16 @@ import mysql.connector
 from kafka import KafkaProducer
 import json
 from datetime import datetime
+from fastavro import parse_schema,schemaless_writer
+from io import BytesIO
 
+
+with open("../schemas/product.avsc", "r") as file:
+    schema = json.load(file)
+
+parsed_schema =parse_schema(schema)
+
+print("Avro schema loaded successfully !")
 
 with open("last_timestamp.txt", "r") as file:
     last_read_timestamp = datetime.fromisoformat(file.read().strip())
@@ -31,20 +40,35 @@ rows =cursor.fetchall()
 
 producer = KafkaProducer(
     bootstrap_servers= "localhost:9092",
-    value_serializer= lambda x: json.dumps(x).encode("utf-8")
+    key_serializer = lambda key: str(key).encode("utf-8"),
+    value_serializer= lambda value: value
 )
 
 #sqls each product is been sent to kafka
-for row in rows:
-    product ={
-        "id" : row[0],
-        "product_name" : row[1],
-        "category" : row[2],
-        "price": float(row[3]),
-        "last_update":str(row[4])
-    }
 
-    producer.send("Producer_topic", value=product)
+if rows:
+    for row in rows:
+        product ={
+            "id" : row[0],
+            "product_name" : row[1],
+            "category" : row[2],
+            "price": float(row[3]),
+            "last_updated":str(row[4])
+        }
+
+    #this creating a temporary place in memory to store bytes
+    buffer = BytesIO()
+    print("Product being serialized:", product)
+    schemaless_writer(buffer,parsed_schema,product)
+
+    # the above line of code means "Take my product dictionary and serialize it into Avro binary format using parsed_schema, then put the result into buffer"
+
+    avro_data = buffer.getvalue()
+
+    print("Avro serialization successfull !")
+    print("Avro data: ", avro_data)
+
+    producer.send("product_updates",key=product["id"], value=avro_data)
 
 
 producer.flush()
