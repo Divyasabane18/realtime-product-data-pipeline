2761fa2825ff2a9059d602d81f6f4cb317c288f2
Divyasaban18 <divyasabane@gmail.com>
2026-09-19 14:36:25 +0530

    complete kafka product data pipeline

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -2,6 +2,13 @@ from kafka import KafkaConsumer
 import json 
 from fastavro import parse_schema, schemaless_reader
 from io import BytesIO
+import sys 
+
+consumer_id = sys.argv[1]
+output_file = f"../output/consumer_{consumer_id}.json"
+print("Output file:", output_file)
+print("Consumer ID: " ,consumer_id)
+
 
 with open("../schemas/product.avsc", "r") as file:
     schema = json.load(file)
@@ -23,4 +30,14 @@ print("Connected to kafka topic")
 for message in consumer:
     buffer = BytesIO(message.value)
     product = schemaless_reader(buffer, parsed_schema)
+
+    #tranform data 
+    product["category"] = product["category"].upper()
+
+    if product["category"] == "ELECTRONICS":
+        product["price"] = round(product["price"] * 0.90 ,2)
+        
+
     print("Received product: ", product)
+    with open(output_file, "a", encoding="utf-8") as file:
+        file.write(json.dumps(product)+ "\n")
